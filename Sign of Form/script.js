21274bfc609c4a88135cc4053a6d5f5dd7a53dc4
Jino0403 <jinho9203@naver.com>
2024-01-14 03:06:48 +0900

    docs: javascript 및 css 수정, 구현완료

--- Sign of Form/script.js
+++ Sign of Form/script.js
@@ -1,1 +1,6 @@
-test
+const container = document.querySelector('.container')
+const signUpBtn = document.querySelector('.green-bg button')
+
+signUpBtn.addEventListener('click', () => {
+  container.classList.toggle('change')
+})
