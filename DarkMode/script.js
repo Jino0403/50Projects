53ad537ca63cfc7b9cd7e2f76201c0d8259a362a
Jino0403 <jinho9203@naver.com>
2024-01-25 01:44:29 +0900

    feat: darkMode 완료

--- DarkMode/script.js
+++ DarkMode/script.js
@@ -1,1 +1,8 @@
-fitstSetting
+const toggle = document.querySelector('.toggle')
+const container = document.querySelector('.container')
+
+toggle.addEventListener('click', () => {
+  container.classList.toggle('dark')
+    ? (toggle.firstElementChild.className = 'far fa-moon')
+    : (toggle.firstElementChild.className = 'far fa-sun')
+})
